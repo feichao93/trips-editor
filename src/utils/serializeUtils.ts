6b45673cd55a122d62b058138862515c054096a0
shinima <842351815@qq.com>
2018-02-11 19:38:48 +0800

    [chore] Refine utils/serializeUtils.ts

--- src/utils/serializeUtils.ts
+++ src/utils/serializeUtils.ts
@@ -1,44 +1,47 @@
-import { List, Seq, Set } from 'immutable'
-import { Item, PolygonItem, PolylineItem, State } from '../interfaces'
+import { Seq, Set } from 'immutable'
+import { Item, ItemId, PolygonItem, PolylineItem, State } from '../interfaces'
+import { isPolygonItem, isPolylineItem } from './common'
+
+function getImgTypeString(item: Item) {
+  if (isPolygonItem(item)) return 'PolygonItem'
+  if (isPolylineItem(item)) return 'PolylineItem'
+  throw new Error('Invalid item.')
+}
+
+function getImgTypeConstructor(name: string) {
+  if (name === 'PolygonItem') return PolygonItem
+  if (name === 'PolylineItem') return PolylineItem
+  throw new Error('Invalid type name.')
+}
 
 const serializeUtils = {
   toJS(state: State) {
-    const object = state
-      .update('items', items =>
-        items.map(item => {
-          const object = item.toJS()
-          if (item instanceof PolygonItem) {
-            object.type = 'PolygonItem'
-          } else if (item instanceof PolylineItem) {
-            object.type = 'PolylineItem'
-          } // other types of items cannot be serialized
-          return object
-        }),
-      )
-      .toJS()
-    object.items = Object.values(object.items).filter(Boolean)
-    const validItemIdSet = Set(object.items.map((item: any) => item.id))
-    object.zlist = object.zlist.filter((itemId: number) => validItemIdSet.has(itemId))
-    return object
+    const items = state.items
+      .filter(item => isPolygonItem(item) || isPolylineItem(item))
+      .map(item => ({
+        type: getImgTypeString(item),
+        ...item.toObject(),
+      }))
+      .valueSeq()
+      .toArray()
+
+    const validItemIdSet = Set(items.map((item: any) => item.id))
+    const zlist = state.zlist.filter((itemId: ItemId) => validItemIdSet.has(itemId)).toArray()
+
+    return { items, zlist }
   },
 
   fromJS(object: any) {
     const items = Seq(object.items)
       .toIndexedSeq()
-      .map((item: any) => {
-        if (item.type === 'PolygonItem') {
-          return PolygonItem.fromJS(item)
-        } else if (item.type === 'PolylineItem') {
-          return PolylineItem.fromJS(item)
-        } else {
-          throw new Error('This object cannot be converted to Item')
-        }
-      })
+      .map((item: any) => getImgTypeConstructor(item.type).fromJS(item))
       .toMap()
       .mapKeys((_: any, item: Item) => item.id)
-    const zlist: any = List(object.zlist)
+    const zlist: any = Seq(object.zlist)
+      .toIndexedSeq()
+      .toList()
 
-    return new State({ items, zlist })
+    return new State({ items, zlist /* use default selMode and selIdSet */ })
   },
 }
 
