84a8cb62e13be99613dbb8a657e495b8a488cb0d
shinima <842351815@qq.com>
2018-02-19 21:27:04 +0800

    [enhance] Refine ToggleLockAction in multi-select mode.

--- src/actions/ToggleLockAction.ts
+++ src/actions/ToggleLockAction.ts
@@ -1,28 +1,39 @@
+import { Set } from 'immutable'
 import { not } from 'ramda'
-import { Action, State, AppHistory } from '../interfaces'
+import { Action, AppHistory, ItemId, State } from '../interfaces'
 
 export default class ToggleLockAction extends Action {
-  message: string
+  lock: boolean
+  itemIdSet: Set<ItemId>
 
   prepare(h: AppHistory): AppHistory {
-    const itemIdStr = h.state
-      .sitems()
+    const sitems = h.state.sitems()
+    this.lock = sitems.some(item => !item.locked)
+    this.itemIdSet = sitems
+      .filter(item => item.locked !== this.lock)
+      .map(item => item.locked)
       .keySeq()
-      .join(',')
-    this.message = `Toggle lock. items={${itemIdStr}}`
+      .toSet()
+
     return h
   }
 
   next(state: State) {
-    const item = state.sitem()
-    return state.update('items', items => items.set(item.id, item.update('locked', not)))
+    const nextItems = state.sitems().map(item => item.set('locked', this.lock))
+    return state.update('items', items => items.merge(nextItems))
   }
 
   prev(state: State) {
-    return this.next(state)
+    const prevItems = state.items
+      .filter(item => this.itemIdSet.has(item.id))
+      .map(item => item.update('locked', not))
+
+    return state.update('items', items => items.merge(prevItems))
   }
 
   getMessage() {
-    return this.message
+    const itemIdStr = this.itemIdSet.keySeq().join(',')
+    const actionStr = this.lock ? 'Lock' : 'Unlock'
+    return `${actionStr} items. items={${itemIdStr}}`
   }
 }
